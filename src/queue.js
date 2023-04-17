c50ba2e61203df5aac40d0df2e5be5d00934d4e3
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-04-17 14:38:48 +0200

    fix: queue

--- src/queue.js
+++ src/queue.js
@@ -1,20 +1,46 @@
+class QueueNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
 class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
+
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const newNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return null;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return value;
   }
-}
+  
+
 
 module.exports = {
   Queue
